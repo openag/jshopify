911b6aa6f17c809653da20bd86ce15a9ab3d670d
Andrei Maus <andreimaus@gmail.com>
2019-02-10 15:33:43 +0100

    wip

--- apps/shopify-change-listener-app/src/main/java/openag/shopify/apps/cl/mongo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package openag.shopify.apps.cl.mongo;
-
-import openag.shopify.domain.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, Long> {
-}
